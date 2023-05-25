abd8e916c46e2644d1c9e2a9e2e9680924ce6aae
hyperpulsor <rakanfasya@gmail.com>
2023-05-25 20:17:16 +0700

    FIX Dashboard for Atlet

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,48 @@ import psycopg2
 
 
 def show_dashboard(request):
-    return render(request, 'dashboard.html')
+    cursor = connection.cursor()
+    query_select = """
+    SELECT *
+    FROM ATLET A, POINT_HISTORY P
+    WHERE A.id = %s AND A.id = P.id_atlet
+    """
+
+    query_select_2 = """
+    SELECT MAX(P.total_point)
+    FROM ATLET A, POINT_HISTORY P
+    WHERE A.id = %s AND A.id = P.id_atlet
+    """
+
+    query_select_3 = """
+    SELECT M.nama
+    FROM ATLET_PELATIH AP, PELATIH P, MEMBER M
+    WHERE AP.ID_ATLET = %s AND AP.ID_pelatih = P.ID AND P.ID = M.ID
+    """
+
+    query_select_4 = """
+    SELECT ID_Atlet
+    FROM ATLET_KUALIFIKASI AK
+    WHERE AK.ID_ATLET = %s
+    """
+
+    nama = request.COOKIES.get("nama")
+    email = request.COOKIES.get("email")
+    cursor.execute(query_select, (request.COOKIES.get("id"),))
+    data_atlet = cursor.fetchall()[0]
+    cursor.execute(query_select_2, (request.COOKIES.get("id"),))
+    poin = cursor.fetchone()[0]
+    cursor.execute(query_select_3, (request.COOKIES.get("id"),))
+    nama_pelatih = cursor.fetchone()[0]
+    cursor.execute(query_select_4, (request.COOKIES.get("id"),))
+    status = cursor.fetchone()[0]
+    if (status is None):
+        status = "Not Qualified"
+    else :
+        status = "Qualified"
+    context = {"data_atlet":data_atlet, "nama":nama, "email":email, "poin":poin, "pelatih":nama_pelatih
+               ,"status":status}
+    return render(request, 'dashboard.html', context=context)
 
 
 def show_dashboard_pelatih(request):
